78dea57044a2cfb1a522a89910337d510acb40f6
Wangpf <1204981117@qq.com>
2021-05-14 20:59:57 +0800

    实现 级联选择器的数据绑定

--- src/components/Goods/GoodsParams.vue
+++ src/components/Goods/GoodsParams.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <el-card>
-      <el-alert title="只允许为第三级分类设置相关参数" type="warning" show-icon></el-alert>
+      <el-alert title="只允许为第三级分类设置相关参数" type="warning" show-icon :closable="false"></el-alert>
       <el-row class="cat_options">
         <el-col>
           <span>选择商品分类：</span>
+          <el-cascader
+            v-model="selectedCateKeys"
+            :options="cateList"
+            :props="{ expandTrigger: 'hover',value:'cat_id',label:'cat_name'}"
+            @change="handleChange"></el-cascader>
         </el-col>
       </el-row>
     </el-card>
@@ -16,7 +21,8 @@ export default {
   name: 'GoodsParams',
   data () {
     return {
-      cateList: []
+      cateList: [],
+      selectedCateKeys: []
     }
   },
   created () {
@@ -28,6 +34,14 @@ export default {
       if (res.meta.status !== 200) { return this.$message.error(`${res.meta.msg}`) }
       this.cateList = res.data
       console.log(this.cateList)
+    },
+    handleChange () {
+      if (this.selectedCateKeys.length < 3) {
+        this.selectedCateKeys = []
+        this.$message.warning('只允许为第三级分类设置相关参数')
+        return
+      }
+      console.log(this.selectedCateKeys)
     }
   }
 }
