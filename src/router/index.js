c5d7090142e79888bb4fe39ae4aefb98652ad6f8
Wangpf <1204981117@qq.com>
2021-05-15 20:15:25 +0800

    解决多次点击导致路由重复问题

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,12 @@ import VueRouter from 'vue-router'
 import { routes } from './routes'
 
 Vue.use(VueRouter)
-
+// 获取原型对象上的push函数
+const originalPush = VueRouter.prototype.push
+// 修改原型对象中的push方法
+VueRouter.prototype.push = function push (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 const router = new VueRouter({
   routes
 })
