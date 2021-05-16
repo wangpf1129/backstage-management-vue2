c9ba8f95c08d9792e95313cbf506b52d1a5068d2
Wangpf <1204981117@qq.com>
2021-05-16 17:04:54 +0800

    自定制首页标题

--- vue.config.js
+++ vue.config.js
@@ -20,6 +20,10 @@ module.exports = {
     // 开发模式
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
     })
   }
 }
